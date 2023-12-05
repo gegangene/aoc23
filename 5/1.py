8b4d1fdd40ec6853d7ca8e7e9e4b507934368a15
ggn <dev@ggngnn.net>
2023-12-06 00:18:41 +0100

    finally works as it should :3

--- 5/1.py
+++ 5/1.py
@@ -3,7 +3,7 @@ currentTab=0
 
 tabs=[]
 result=[]
-seedsNo=4
+seedsNo=20
 with open("input5.txt","r") as fileHandle:
 	for line in fileHandle:
 		line=line.strip()
@@ -14,7 +14,6 @@ with open("input5.txt","r") as fileHandle:
 				if currentTab>0:
 					for element in tabs[currentTab-1].values():
 						tabs[currentTab][element]=element
-				# print(currentTab)
 		lineList=line.split(" ")
 		if len(lineList)==seedsNo:
 			for i in range(seedsNo):
@@ -23,9 +22,6 @@ with open("input5.txt","r") as fileHandle:
 			lineList=list(map(int,lineList))
 			for value in tabs[currentTab-1].values():
 				if value in range(lineList[1],lineList[1]+lineList[2]):
-			# for ii in range(0,int(lineList[2])):
-			# 	if int(lineList[1])+ii in tabs[currentTab-1].values():
 					tabs[currentTab][value]=value+(lineList[0]-lineList[1])
 	
 print(min(tabs[7].values()))
-# print(tabs)
